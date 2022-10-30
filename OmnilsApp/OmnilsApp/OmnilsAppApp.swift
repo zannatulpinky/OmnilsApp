8b7d25ccc6d33b8a5534f27c080c705a1772662f
zannatul-fardous <zannat12.fardous@gmail.com>
2022-10-30 20:43:38 +0100

    Add firebasefirestore and authentication

--- OmnilsApp/OmnilsApp/OmnilsAppApp.swift
+++ OmnilsApp/OmnilsApp/OmnilsAppApp.swift
@@ -6,9 +6,15 @@
 //
 
 import SwiftUI
+import FirebaseCore
 
 @main
 struct OmnilsAppApp: App {
+    
+    init() {
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
