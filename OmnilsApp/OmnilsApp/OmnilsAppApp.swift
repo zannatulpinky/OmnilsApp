3556f82d5ccb1abab0b2361c117ef9966017fae0
zannatul-fardous <zannat12.fardous@gmail.com>
2022-10-30 19:28:47 +0100

    First

---
+++ OmnilsApp/OmnilsApp/OmnilsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  OmnilsAppApp.swift
+//  OmnilsApp
+//
+//  Created by Zannatul Pinky on 2022-10-30.
+//
+
+import SwiftUI
+
+@main
+struct OmnilsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
