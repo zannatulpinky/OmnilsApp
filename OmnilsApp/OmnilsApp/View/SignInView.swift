ab3ececea0dec07caab5be897deaef582c9ba203
zannatul-fardous <zannat12.fardous@gmail.com>
2022-11-02 11:17:21 +0100

    signIn View design

--- OmnilsApp/OmnilsApp/View/SignInView.swift
+++ OmnilsApp/OmnilsApp/View/SignInView.swift
@@ -7,9 +7,54 @@
 
 import SwiftUI
 
+let gradient = LinearGradient(colors: [Color.white,Color.green], startPoint: .top, endPoint: .bottom)
+
 struct SignInView: View {
+    
+    @State var email = ""
+    @State var password = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        ZStack {
+            gradient
+                .opacity(0.25)
+                .ignoresSafeArea()
+            
+            VStack() {
+                Spacer()
+                
+                Text ("OMNILS")
+                    .font(.largeTitle)
+                    .foregroundColor(Color.green)
+                    .fontWeight(.heavy)
+                    Spacer()
+                
+                TextField("Email", text: $email)
+                    .padding()
+                    .background()
+                    .cornerRadius(5.0)
+                    .padding(.bottom, 20)
+                
+                SecureField("Password", text: $password)
+                    .padding()
+                    .background()
+                    .cornerRadius(5.0)
+                    .padding(.bottom, 20)
+                
+                Button(action: {
+                    
+                }, label: {
+                    Text("LogIn")
+                        .padding()
+                        .background(.green)
+                        .cornerRadius(5.0)
+                        .foregroundColor(.white)
+                })
+                Spacer()
+            }
+            .padding()
+        }
     }
 }
 
